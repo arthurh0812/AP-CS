1b34fd04d5e71a2e10278fcbfc533a6585df2fa0
Arthur Hennig <ahennig@Dar30437.local>
2021-08-16 22:43:35 -0400

    Initial commit

---
+++ 1-Primitives/src/ap_cs/primitives/first_program/Main.java
@@ -0,0 +1,144 @@
+package ap_cs.primitives.first_program;
+
+public class Main {
+    public static void main(String[] args) {
+        a1();
+        a2();
+        a5();
+        a6();
+        a7();
+        a8();
+        a9();
+        a10();
+        a11();
+        a12();
+        a13();
+        a14();
+        a15();
+        a16();
+        a18();
+    }
+
+    private static void a1() {
+        System.out.println("Hello World!");
+        /*
+            Hello World
+         */
+    }
+
+    private static void a2() {
+        System.out.println("Hello Sunshine");
+        /*
+            Hello Sunshine
+         */
+    }
+
+    private static void a5() {
+        System.out.println("Hello CS");
+        /*
+            Hello CS
+         */
+    }
+
+    private static void a6() {
+        System.out.println(1 + 9);
+        /*
+            10
+         */
+    }
+
+    private static void a7() {
+        System.out.println("1 + 9");
+        /*
+            1 + 9
+         */
+    }
+
+    private static void a8() {
+        System.out.println("Hello ");
+        System.out.println();
+        System.out.println("World");
+        /*
+            Hello
+
+            World
+         */
+    }
+
+    private static void a9() {
+        System.out.print("Welcome to");
+        System.out.print(" the world ");
+        System.out.println("of");
+        System.out.print(" Java");
+        /*
+            Welcome to the world of
+             Java
+         */
+    }
+
+    private static void a10() {
+        System.out.print("pine");
+        System.out.println("apple");
+        System.out.println("pine");
+        System.out.print("apple");
+        /*
+            pineapple
+            pine
+            apple
+         */
+    }
+
+    private static void a11() {
+        System.out.print("aa");
+        System.out.print("  bb");
+        System.out.println("cc");
+        System.out.print("  dd");
+        /*
+
+         */
+    }
+
+    private static void a12() {
+        System.out.print("aa");
+        System.out.print("  bb");
+        System.out.println();
+        System.out.print("  dd");
+        /*
+            aa  bb
+              dd
+         */
+    }
+
+    private static void a13() {
+        System.out.println(5 + 5);
+        /*
+            10
+         */
+    }
+
+    private static void a14() {
+        System.out.println("5 + 5");
+        /*
+            5+5
+         */
+    }
+
+    private static void a15() {
+        System.out.println("5" + 5);
+        /*
+            55
+         */
+    }
+
+    private static void a16() {
+        System.out.print("print");
+        System.out.println();
+        System.out.print("me");
+    }
+
+    private static void a18() {
+        System.out.println("print");
+        System.out.println("this");
+        System.out.print("too");
+    }
+}
